d0d9779ce91ba063629c9938bd29c460b1f4749c
Terr <terrenceyang06@gmail.com>
2025-09-10 18:05:38 -0400

    Create boilerplate

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      "/v1": {
+        target: "https://fra.cloud.appwrite.io",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
